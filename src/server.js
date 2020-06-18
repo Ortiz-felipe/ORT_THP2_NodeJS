aeee1f13b4e01a61cc7589dae871ccc368926d68
Felipe Ortiz Correa <Felipe.Ortiz.Correa@ey.com>
2020-06-18 19:57:42 -0300

    Fixes on server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,2 @@
 import app from './app.js';
-
-const MailingFactory = require('./EmailModule/MailingFactory');
-
-const mailFactory = new MailingFactory();
-const mailService = mailFactory.getMailingService();
-
-console.log(mailService);
-
-
 app.listen(3000, () => console.log('Escuchando en puerto 3000'));
