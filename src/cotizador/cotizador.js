c93571d355d26479a1f71195baa793122a753531
unknown <nancy.helenam@gmail.com>
2020-06-28 22:29:48 -0300

    descomento validacion del cotizador

--- src/cotizador/cotizador.js
+++ src/cotizador/cotizador.js
@@ -9,9 +9,9 @@ class Cotizador {
   async convertirPesosADolares(montoEnPesosAConvertir) {
     // no permitimos decimales,strings o un entero negativo ya que en tal caso la api devolvera el
     // monto de $1, tampoco el 0 ya que en tal caso la api devuelve un error
-    /* if (!Number.isInteger(montoEnPesosAConvertir) || montoEnPesosAConvertir < 1) {
-            throw new CotizacionError(400, 'El monto a convertir debe ser un entero positivo')
-        } */
+    if (!Number.isInteger(montoEnPesosAConvertir) || montoEnPesosAConvertir < 1) {
+      throw new CotizacionError(400, 'El monto a convertir debe ser un entero positivo');
+    }
 
     const url = `https://api.cambio.today/v1/quotes/ARS/USD/json?quantity=${montoEnPesosAConvertir}&key=${this.key}`;
     let montoRecibido;
