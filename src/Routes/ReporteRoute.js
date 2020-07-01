3fe7a1645719ea6b431ac4afb30420ce3a1a215e
Felipe Ortiz Correa <Felipe.Ortiz.Correa@ey.com>
2020-06-30 22:50:14 -0300

    Se agrega la documentacion correspondiente a ReporteRoute

--- src/Routes/ReporteRoute.js
+++ src/Routes/ReporteRoute.js
@@ -5,7 +5,27 @@ function getReportesRouter() {
   const router = express.Router();
 
   const reportesAPI = getReportesApi();
-
+  /**
+ * @api {get} generarReporte/mensual Generara un reporte con las ganancias del mes
+ *
+ * @apiVersion 0.1.0
+ * @apiName Mensual
+ * @apiGroup GenerarReporte
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *
+ * @apiErrorExample {json} ConvertionError
+ *    HTTP/1.1 500 Internal Server Error
+ *    {
+ *      "error": "Error al convertir el monto solicitado"
+ *    }
+ * @apiErrorExample {json} CurrencyNotNegative
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "error": "El monto a convertir no puede ser negativo"
+ *    }
+ */
   router.get('/mensual', async (req, res) => {
     try {
       const respuesta = await reportesAPI.generarReporteMensual();
