6613b36e7641a81456aa5d7a92d22e3918555f19
unknown <nancy.helenam@gmail.com>
2020-07-01 22:14:11 -0300

    edité docu

--- src/Routes/ReporteRoute.js
+++ src/Routes/ReporteRoute.js
@@ -6,7 +6,7 @@ function getReportesRouter() {
 
   const reportesAPI = getReportesApi();
   /**
- * @api {get} generarReporte Generara un reporte con las ganancias del mes
+ * @api {get} Generarreportes/mensual Generara un reporte con las ganancias del mes
  *
  * @apiVersion 0.1.0
  * @apiName Mensual
