765464d10fe84e021e646986c6eb8faf95747be5
Felipe Ortiz Correa <Felipe.Ortiz.Correa@ey.com>
2020-07-01 23:31:26 -0300

    Se agrega el envio del mail al CU_confirmarReserva

--- src/CasosDeUso/CU_confirmarReserva.js
+++ src/CasosDeUso/CU_confirmarReserva.js
@@ -1,18 +1,35 @@
+import Email from '../models/email.js';
+
 class CU_confirmarReserva {
-  constructor(reservasRepository) {
+  constructor(reservasRepository, mailService) {
     this.reservasRepository = reservasRepository;
+    this.mailService = mailService;
   }
 
   async run(id) {
-    const reservaEncontrada = await this.reservasRepository.obtenerPorId(id);
-    if (reservaEncontrada) {
-      reservaEncontrada.estadoReserva = true;
-      await this.reservasRepository.actualizar(id, reservaEncontrada);
-    } else {
-      throw {
-        error: 'Id no encontrado',
-        status: 404,
-      };
+    try {
+      const reservaEncontrada = await this.reservasRepository.obtenerPorId(id);
+      if (reservaEncontrada) {
+        reservaEncontrada.estadoReserva = true;
+        await this.reservasRepository.actualizar(id, reservaEncontrada);
+        const cuerpoMensaje = `Hola ${reservaEncontrada.nombre}. Se ha confirmado tu reserva para el dia ${reservaEncontrada.fecha}`;
+        const email = new Email(reservaEncontrada.Email, 'Confirmacion de Reserva', cuerpoMensaje);
+        this.mailService.send(email);
+      } else {
+        throw Error({ name: 'repositoryError', message: 'Id no encontrado' });
+      }
+    } catch (error) {
+      if (error.name === 'repositoryError') {
+        throw {
+          error: 'Id no encontrado',
+          status: 404,
+        };
+      } else {
+        throw {
+          error: error.name,
+          message: error.message,
+        };
+      }
     }
   }
 }
